0a033f7f9e9cd031c06f5a3182855c8dae7d5bee
Maria Stachovski <maryia.jegorova@gmail.com>
2024-05-21 20:30:38 +0200

    feat: create layout components

---
+++ src/components/Header.tsx
@@ -0,0 +1,7 @@
+const Header = () => {
+return  <header className='header'>
+<img src='https://e7.pngegg.com/pngimages/779/61/png-clipart-logo-idea-cute-eagle-leaf-logo.png'/>
+</header>
+}
+
+export default Header;
