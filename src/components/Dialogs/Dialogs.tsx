b2ea4427992e21580efa29b9557baa332ccfae82
Maria Stachovski <maryia.jegorova@gmail.com>
2024-05-24 23:20:04 +0200

    feat: add data

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -3,14 +3,32 @@ import DialogItem from './DialogItem/DialogItem'
 import Message from './Message/Message'
 
 const Dialogs = () => {
+const dialogData = [
+  {name: 'Masha', id:'1'},
+  {name: 'Sasha', id:'2'},
+  {name: 'Pasha', id:'3'}
+] 
+
+const messageData = [
+  {message: 'Hi', id:'1'},
+  {message: 'Yo', id:'2'},
+  {message: 'Tschuss', id:'3'}
+] 
+
+
     return <div className={styles.dialogs}>
         <div className={styles.dialogsItems}>
-          <DialogItem name='Masha' id='1'/>
-          <DialogItem name='Pasha' id='2'/>
+          {dialogData.map((dialog) => {
+            return <DialogItem key={dialog.id} name={dialog.name} id={dialog.id}/>
+          })}
+          
         </div>
 
         <div className={styles.messages}>
-          <Message message="Hi"/>
+          {messageData.map((message) => {
+            return <Message key={message.id} message={message.message}/>
+          })}
+          
         </div>
     </div>
 }
--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -2,6 +2,12 @@ import styles from './MyPosts.module.scss'
 import Post from './Post/Post';
 
 const MyPosts = () => {
+
+  const postsData = [
+    {message: 'Hi', id:'1', like:'10'},
+    {message: 'How are you?', id:'2', like:'12'},
+  ] 
+
     return <><div className={styles.postsBlock}>
       My posts
       <div>
@@ -11,8 +17,11 @@ const MyPosts = () => {
       </div>
     </div>
     <div className={styles.posts}>
-    <Post message='Hi' like='10' />
-    <Post message='How are you?' like='42'/>
+      {postsData.map((post) => {
+        return   <Post key={post.id} message={post.message} like={post.like} />
+      })}
+  
+    
     </div>
    
     </>
